8004d76975c5851c82d3c68fe292ba787711b940
Glitch (seatgeek-codenation-rockpaperscissors) <none>
2019-11-17 22:04:16 +0000

    🐜🌅 Checkpoint ./js/script.js:8584856/656

--- js/script.js
+++ js/script.js
@@ -13,15 +13,34 @@ $("#shoot").click(function(){
   num = Math.ceil(num);
   console.log(num);
   // random number generator
-  
-  // assigning empty variable to be filled later
+  // rock=1
+  // paper=2
+  // scissors=3
   if(num===1||userchoice==="rock"){
     
      }
-  else if(num===2){
+  else if(num===1||userchoice==="paper"){
     
      }
-  else if(num===3){
+  else if(num===1||userchoice==="scissors"){
+    
+     }
+  else if(num===2||userchoice==="ro"){
+    
+     }
+  else if(num===2||userchoice==="scissors"){
+    
+     }
+  else if(num===2||userchoice==="paper"){
+    
+     }
+  else if(num===3||userchoice==="scissors"){
+    
+     }
+  else if(num===3||userchoice==="paper"){
+    
+     }
+  else if(num===3||userchoice==="scissors"){
     
      }
   // assigning names to numbers / filling in variable 
