bfc7af0221cae47bbdba872742cf3ebe56af99df
Glitch (seatgeek-codenation-rockpaperscissors) <none>
2019-11-17 22:39:22 +0000

    🏏👼 Checkpoint ./js/script.js:8584856/901

--- js/script.js
+++ js/script.js
@@ -22,13 +22,19 @@ $("#shoot").click(function(){
     $("#result").text("Draw");
      }
   else if(num===1||userchoice==="paper"){
-    
+    $("#userChoice").text("Paper");
+    $("#computerChoice").text("Rock");
+    $("#result").text("You Win!");
      }
   else if(num===1||userchoice==="scissors"){
-    
+    $("#userChoice").text("Scissors");
+    $("#computerChoice").text("Rock");
+    $("#result").text("Computer Wins");
      }
   else if(num===2||userchoice==="rock"){
-    
+    $("#userChoice").text("Rock");
+    $("#computerChoice").text("Paper");
+    $("#result").text("Computer Wins");
      }
   else if(num===2||userchoice==="paper"){
     $("#userChoice").text("Paper");
@@ -36,13 +42,19 @@ $("#shoot").click(function(){
     $("#result").text("Draw");
      }
   else if(num===2||userchoice==="scissors"){
-    
+    $("#userChoice").text("Scissors");
+    $("#computerChoice").text("Paper");
+    $("#result").text("You Win!");
      }
   else if(num===3||userchoice==="rock"){
-    
+    $("#userChoice").text("Rock");
+    $("#computerChoice").text("Scissors");
+    $("#result").text("You Win!");
      }
   else if(num===3||userchoice==="paper"){
-    
+    $("#userChoice").text("Paper");
+    $("#computerChoice").text("Scissors");
+    $("#result").text("Computer Wins");
      }
   else if(num===3||userchoice==="scissors"){
     $("#userChoice").text("Scissors");
