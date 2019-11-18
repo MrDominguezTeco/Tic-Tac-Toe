3cb8d795785f6aa1710341d8889d2d70cc2da9b2
Glitch (seatgeek-codenation-rockpaperscissors) <none>
2019-11-18 05:29:41 +0000

    💼💨 Checkpoint ./js/script.js:8584856/3794

--- js/script.js
+++ js/script.js
@@ -6,55 +6,49 @@
 /* global $ */
 
 // DOCUMENT READY FUNCTION BELOW
-$("#shoot").click(function(){
-  let userchoice=$("#input").val();
+$("#shoot").click(function() {
+  let userchoice = $("#input").val();
   $("#userChoice").text(userchoice);
-  // gets value from input
-  let num = Math.random()*3;
+  // gets value from input and prints text
+  let num = Math.random() * 3;
   num = Math.ceil(num);
   console.log(num);
   // random number generator
   // rock=1
   // paper=2
   // scissors=3
-  if(num===1||userchoice==="rock"){
+  if (num === 1 || userchoice === "rock") {
     $("#computerChoice").text("Rock");
     $("#result").text("Draw");
-     }
-  else if(num===1||userchoice==="paper"){
+  } else if (num === 1 || userchoice === "paper") {
     $("#computerChoice").text("Rock");
     $("#result").text("You Win!");
-     }
-  else if(num===1||userchoice==="scissors"){
+  } else if (num === 1 || userchoice === "scissors") {
     $("#computerChoice").text("Rock");
     $("#result").text("Computer Wins");
-     }
-  else if(num===2||userchoice==="rock"){
+  } else if (num === 2 || userchoice === "rock") {
     $("#computerChoice").text("Paper");
     $("#result").text("Computer Wins");
-     }
-  else if(num===2||userchoice==="paper"){
+  } else if (num === 2 || userchoice === "paper") {
     $("#computerChoice").text("Paper");
     $("#result").text("Draw");
-     }
-  else if(num===2||userchoice==="scissors"){
+  } else if (num === 2 || userchoice === "scissors") {
     $("#computerChoice").text("Paper");
     $("#result").text("You Win!");
-     }
-  else if(num===3||userchoice==="rock"){
+  } else if (num === 3 || userchoice === "rock") {
     $("#computerChoice").text("Scissors");
     $("#result").text("You Win!");
-     }
-  else if(num===3||userchoice==="paper"){
+  } else if (num === 3 || userchoice === "paper") {
     $("#computerChoice").text("Scissors");
     $("#result").text("Computer Wins");
-     }
-  else if(num===3||userchoice==="scissors"){
+  } else if (num === 3 || userchoice === "scissors") {
     $("#computerChoice").text("Scissors");
     $("#result").text("Draw");
-     }
-  // assigning names to numbers / filling in variable 
+  }
+  // assigning names to numbers / filling in variable
   // if( opp==="rock"||userchoice==="" ){
-     
-     // }
+
+  // }
+  console.log(userchoice);
+  console.log(num);
 });
